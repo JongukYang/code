ea3635b696f86c7338dec9707b5e6a7342c39c93
JongukYang <didwhddnr123@naver.com>
2022-07-10 17:47:37 +0900

    master

--- devsns2/devsns2/settings.py
+++ devsns2/devsns2/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = mySECRET_KEY
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'devsns2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-myDATABASES
+DATABASES = myDATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
